079b4a96785fb414aab0499ff7780bfa02cda1fd
drealdumore <sireroyce1@gmail.com>
2024-10-27 08:53:15 +0100

    chore:: logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
+import Dropzone from "@/components/dropzone";
+
 export default function Home() {
   return (
     <div className="space-y-16 pb-8">
       <div className="space-y-6">
         <h1 className="text-5xl font-medium text-center">Pulse</h1>
-        <p className="text-light text-center md:px-24 xl:px-44 2xl:px-52">
+        <p className="text-light text-center">
           Powerful file conversion app that supports a wide range of formats for
           images, videos, and audio. Experience fast and reliable conversions
           with a sleek user interface.
         </p>
       </div>
+
+      <Dropzone />
     </div>
   );
 }
